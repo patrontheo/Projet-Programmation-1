2f78589a113d557ad4fd53cc50802d749cee4842
Romain Palazzo <romainpalazzo@tsf-476-wpa-5-023.epfl.ch>
2018-03-09 14:47:17 +0100

    Initial Commit

---
+++ Projet Prog/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Projet Prog
+//
+//  Created by Romain Palazzo on 09.03.18.
+//  Copyright © 2018 Romain Palazzo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
